c9d1353a231c83afee1774ac3eb96461e15b51b9
Serge Oliveira Sardinha <sergeosardinha@gmail.ca>
2023-03-26 09:07:00 -0400

    Initial commit

---
+++ src/main/java/io/sos/lulunotes/LuluNotesApplication.java
@@ -0,0 +1,13 @@
+package io.sos.lulunotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuluNotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuluNotesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/sos/lulunotes/LuluNotesApplicationTests.java
@@ -0,0 +1,13 @@
+package io.sos.lulunotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuluNotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
